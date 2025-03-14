29b4cae8e137898e53977a378bb442677b489dac
MohdRaza710 <mrmraza.710@gmail.com>
2025-03-14 17:20:28 +0300

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`} className="block w-full">
-      <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 w-full max-w-[400px] mx-auto">
+      <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300 w-full max-w-[400px] mx-auto mb-4">
         
         <div className="w-full h-48 sm:h-60">
           <img
