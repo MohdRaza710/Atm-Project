7713873aeeeb6020c78f40a5ebce9468cec9c34d
MohdRaza710 <mrmraza.710@gmail.com>
2025-03-14 15:14:05 +0300

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img width={width} src="https://www.logoai.com/uploads/output/2022/01/16/2fcac87be864706bdc67c7eafe94b87e.jpg?t=1642342896" alt="megaBLog" /></div>
+  )
+}
+
+export default Logo
